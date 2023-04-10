5ed0f5487ee518656d8d51d958949c1390f1a42b
Vesta-nna <anna.dominguez.anna@gmail.com>
2023-04-10 21:51:48 +0200

    change bg and shadow

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const Home = () => (
   <div
     className="flex flex-col lg:flex-row items-center lg:mx-auto lg:my-auto
               max-w-[736px] h-fit justify-center lg:rounded-[32px] bg-white
-              shadow-2xl shadow-darkNavy/10"
+              shadow-2xl shadow-blue/20"
   >
     <Header />
     <Main />
