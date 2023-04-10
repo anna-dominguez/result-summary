305b6616fdf42db76e2dadebad84e13d1e1ba682
Vesta-nna <anna.dominguez.anna@gmail.com>
2023-04-10 21:44:12 +0200

    change bg and shadow

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import Head from 'next/head'
 
 const App = ({ Component, pageProps }) => (
-  <div className="bg-[#ECF2FF] grid w-full h-screen">
+  <div className="grid w-full h-screen">
     <Head>
       <title>Result Summary</title>
     </Head>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const Home = () => (
   <div
     className="flex flex-col lg:flex-row items-center lg:mx-auto lg:my-auto
               max-w-[736px] h-fit justify-center lg:rounded-[32px] bg-white
-              shadow-[0_30px_60px_rgba(61, 108, 236, 0.15)]"
+              shadow-2xl shadow-darkNavy/10"
   >
     <Header />
     <Main />
